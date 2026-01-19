90bbd5ec7889b2de721370dae795fcaa31242a91
09Palak <palaksharmahp16@gmail.com>
2026-01-19 21:00:03 +0530

    Create app.js

---
+++ react-hooks-counter/src/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>
+        React Hooks Counter App
+      </h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
